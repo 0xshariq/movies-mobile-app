e7d852610dd2cfb7868bac8f8effcf353a2bc7f5
Sharique Chaudhary <khanshariq92213@gmail.com>
2025-10-18 12:21:49 +0000

    Assests Added

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-accent text-3xl">Welcome!</Text>
+      
     </View>
   );
 }
