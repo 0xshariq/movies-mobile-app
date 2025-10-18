5b8f95e2131a5f406285697bfa1a2849e98c7b57
Sharique Chaudhary <khanshariq92213@gmail.com>
2025-10-18 18:12:18 +0000

    Routes Created

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,28 @@
 import { Stack } from "expo-router";
 import './globals.css';
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen 
+      name="index"
+      options={
+        {
+          title: 'Home',
+          headerShown: false,
+        }
+      }
+    />
+    <Stack.Screen 
+     name='(tabs)'
+     options: {{
+      headerShown: false
+     }}
+    />
+    <Stack.Screen 
+      name="movie/[id]"
+      options: {{
+      title: "Movie-Detail",
+      headerShown: false
+     }} 
+    />
+  </Stack>;
 }
