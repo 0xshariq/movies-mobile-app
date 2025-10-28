3338b6f35b197819fc686d6b720e516117db24a7
Sharique Chaudhary <khanshariq92213@gmail.com>
2025-10-28 12:21:53 +0000

    Minor changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
 import { Stack } from "expo-router";
 import "./globals.css";
-import Middleware from './_middleware'
-import { AuthProvider } from '@/lib/AuthProvider'
-import { ToastProvider } from '@/lib/ToastProvider'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import Middleware from "./_middleware";
+import { AuthProvider } from "@/lib/AuthProvider";
+import { ToastProvider } from "@/lib/ToastProvider";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
@@ -11,34 +12,35 @@ export default function RootLayout() {
       <AuthProvider>
         <ToastProvider>
           <Middleware />
+          <StatusBar hidden={true} />
           <Stack>
-          <Stack.Screen
-            name="index"
-            options={{
-              title: "Home",
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="(tabs)"
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="(auth)"
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="movie/[id]"
-            options={{
-              title: "Movie-Detail",
-              headerShown: false,
-            }}
-          />
-        </Stack>
+            <Stack.Screen
+              name="index"
+              options={{
+                title: "Home",
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="(tabs)"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="(auth)"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="movie/[id]"
+              options={{
+                title: "Movie-Detail",
+                headerShown: false,
+              }}
+            />
+          </Stack>
         </ToastProvider>
       </AuthProvider>
     </SafeAreaProvider>
